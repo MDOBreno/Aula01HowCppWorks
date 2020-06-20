977634897b5bd711742a7776e402a5056941e327
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-06-20 17:50:27 -0300

    Initial Commit

---
+++ Aula01HowCppWorks/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Aula01HowCppWorks
+//
+//  Created by Breno Medeiros on 20/06/20.
+//  Copyright © 2020 ProgramasBMO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
